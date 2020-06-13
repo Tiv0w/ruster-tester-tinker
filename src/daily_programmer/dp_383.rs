c58dee2a040151987344381bd80ce690cf7d37fe
Tiv0w <t.theomeyer@gmail.com>
2020-06-13 10:11:40 +0200

    :construction: starting the Daily Programmer 384 challenge

---
+++ src/daily_programmer/dp_383.rs
@@ -0,0 +1,19 @@
+struct Necklace {
+    elements: &[char],
+}
+
+fn rotate_one(&mut array: [char]) {
+    &array[1..]
+}
+
+impl Necklace {
+    fn rotate(&mut self, increment: u8) {
+        for counter in [0..increment] {
+            tmp = &self.elements[counter..];
+        }
+    }
+}
+
+pub fn same_necklace() {
+    println!("yes");
+}
